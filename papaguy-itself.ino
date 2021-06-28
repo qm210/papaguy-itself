b7c8b657336fc2ea03bfa8d1c3f9be43a9c0b235
qm210 <quantenmace@gmail.com>
2021-06-28 14:50:00 +0200

    basic stuff

--- papaguy-itself.ino
+++ papaguy-itself.ino
@@ -1,16 +1,20 @@
 #include <ESP32Servo.h>
 
-int pos = 0;
-int servo_pin = 18;
+#define SERIAL_PORT 115200
 
-N_SERVO = 1;
-Servo *surfo;
+#define N_SERVO 1
+Servo surfo[N_SERVO];
+int surfo_pin[N_SERVO] = {
+    18
+};
+
+char message_head;
+short message_body;
 
 enum Action {
   IDLE, SET_SERVO
 };
-int servo_target = 0; // 0 is none, t
-Action action = Action.IDLE;
+Action action = Action::IDLE;
 
 // update frequency - 20Hz - guess I need to adjust the attach() parameters
 
@@ -20,29 +24,49 @@ void setup() {
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
 
-  surfo.setPeriodHertz(50);
-  surfo.attach(servo_pin, 500, 2400); // pin number, min, max microsecond settings
+  for (int s=0; s < N_SERVO; s++) {
+    surfo[s].setPeriodHertz(50);
+    surfo[s].attach(surfo_pin[s], 500, 2400); // pin number, min, max microsecond settings for PWM
+  }
+
+  Serial.begin(SERIAL_PORT);
+  Serial.println("PapaGuy is alive.");
 }
 
 void loop() {
-  listen_for_message();
-  
+  for(int s=0; s < N_SERVO; s++) {
+    listen_for_message(s);
+    execute(s);
+  }
+}
+
+void listen_for_message(int index) {
+    if (!surfo[index].attached()) {
+      action = Action::IDLE;
+      return;
+    }
+    int result = Serial.readBytes(&message_head, 1);
+    Serial.print("RESULT ");
+    Serial.println(result);
+}
+
+void execute(int index) {
+  int current_state, new_state;
   switch (action) {
-    case Action.SET_SERVO:
-      surfo.write(pos);
-    
-    case Action.IDLE:
+    case Action::SET_SERVO:
+      current_state = surfo[index].read();
+      new_state = (int)(((float)message_body / 1024.) * 180.);
+      if (current_state != new_state) {
+        Serial.print("POS ");
+        Serial.print(current_state);
+        Serial.print(" -> ");
+        Serial.println(new_state);
+        surfo.write(new_state);
+      }
+      return;
+
+    case Action::IDLE:
     default:
       return;
   };
-  if (action == Action.IDLE) {
-    return;
-  }
-  for (pos = 0; pos <= 180; pos++) {
-    //delay(5);
-  }
-  for (pos = 180; pos >= 0; pos--) {
-    surfo.write(pos);
-    //delay(5);
-  }
 }
